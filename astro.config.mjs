672266e91648e243f0438ebec70a1f94e7d8518e
Mohit Pujari <86524672+Brave-Programmmer@users.noreply.github.com>
2025-05-26 14:40:20 +0530

    Netlify Switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
-
+// import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://scriptora-rho.vercel.app/',
-  adapter: vercel({ edge: false })  // <-- CRUCIAL
+  // adapter: vercel({ edge: false })  // <-- CRUCIAL
+  adapter: netlify(),
 });
